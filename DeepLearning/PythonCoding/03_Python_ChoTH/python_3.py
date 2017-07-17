eda63bed0d611a49a6e176e7bd9e90e4cfcdfb68
Administrator <meilleur7@gmail.com>
2017-07-17 20:45:00 +0900

    a

--- DeepLearning/PythonCoding/03_Python_ChoTH/python_3.py
+++ DeepLearning/PythonCoding/03_Python_ChoTH/python_3.py
@@ -121,3 +121,105 @@ marxes.count('Harpo')   # 'Harpo' 몇개이니
 snl_skit = ['cheeseburger', 'cheeseburger', 'cheeseburger']
 snl_skit.count('cheeseburger')
 
+# 문자열로 변환하기: join() ★★★★★★★★★★★★★★★★
+# join()은 문자열 메서드, 리스트 메서드가 아니다!!
+# marxes.join(',')가 더 직관적이지만 이렇게 쓸 수 없다.
+# split()와 쓰는 방법도 반대, a.split(',') | (',').join(a)
+# split()는 중간의 구분자를 기준으로 나누겠다는 뜻
+# join()은 중간의 구분자를 기준으로 합치겠다는 뜻
+friends = ['Herry', 'Hermione', 'Ron']
+separator = ' * '
+joined = separator.join(friends)
+joined
+separated = joined.split(separator)
+separated
+separated == friends
+
+# 정렬하기: sort()
+# sort()는 리스트 자체를 내부적으로 정렬한다.
+# sorted()는 리스트의 정렬된 복사본을 반환한다.
+
+marxes = ['Groucho', 'Chico', 'Harpo']
+sorted_marxes = sorted(marxes)
+sorted_marxes   # 문자열의 경우 알파벳 순으로 정렬
+marxes   # 원본리스트는 변하지 않았다.
+
+marxes.sort()
+marxes   # 원본이 바뀌었다.
+
+# 정수와 부동소수점을 혼합해서 쓸 수도 있다. 파이썬이 자동으로 타입을 형변환해주기 때문에..
+numbers = [2,1,4.0,3]
+numbers.sort()
+numbers
+
+numbers = [2,1,4.0,3]
+numbers.sort(reverse=True)
+numbers
+
+# 항목의 개수 얻기
+marxes = ['Groucho', 'Chico', 'Harpo']
+len(marxes)
+
+# 할당: =, 복사: copy()
+a = [1,2,3]
+a
+b = a
+a
+a[0] = 'surprise'   # 항목 바꾸기
+a
+b   # a가 변하면서 같은 객체를 공유하고 있던 b 역시 변하게 된다.
+    # 변수이름은 '스티커'다.
+
+# 스티커가 아닌 따로 객체를 만들고 싶다면 copy()를 사용한다.
+a = [1,2,3]
+b = a.copy()   # 다 같은 복사본을 만드는 방법!
+c = list(a)    # 다 같은 복사본을 만드는 방법!
+d = a[:]       # 다 같은 복사본을 만드는 방법!
+
+a[0] = 'integer lists are boring'
+a
+b   # a와 같은 객체를 공유하지 않는다.
+c   # a와 같은 객체를 공유하지 않는다.
+d   # a와 같은 객체를 공유하지 않는다.
+
+# 튜플(tuple)
+# 임의적인 항목 시퀀스, 리스트와 다르게 튜플을 불변한다. (추가, 삭제, 수정 불가)
+# 튜플은 상수의 리스트라고 할 수 있다.
+# 튜플생성하기
+empty_tuple = ()
+empty_tuple
+
+one_marx = 'Groucho',   # 이렇게 하나의 항목을 튜플변수에 담을 수 있다.
+one_marx
+one_marx = tuple('Groucho')   # 이것은 충격적인 결과!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+one_marx
+# 튜플의 아이템이 하나일 경우 문자열 뒤에 ','를 붙여줌으로써 일반 문자열과 구분해준다.
+# 문자열 뒤에 ','(comma)를 붙여주지 않으면 구별하지 못한다. 즉 a = ('a')이렇게는 불가능하다. (리스트형태는 가능)
+# 두개 이상의 아이템을 할당할 경우에는 마지막 요소에 콤마가 불필요하다. (붙여도 되고)
+marx_tuple = 'Groucho', 'Choco', 'Harpo'   # 문자열변수는 두개 이상의 아이템을 갖는 것이 불가능하기 때문에 ()(괄호)가 불필요하다.
+marx_tuple
+marx_tuple = ('Groucho', 'Choco', 'Harpo') # 이렇게 붙여도 되고
+marx_tuple
+
+# 튜플은 한번에 여러 변수에 할당할 수 있다.
+marx_tuple = ('Groucho', 'Choco', 'Harpo')
+a, b, c = marx_tuple
+a
+b
+c
+# 이것을 튜플 언패킹이라고 한다.
+
+password = 'swordfish'
+icecream = 'tuttifrutti'
+abc = password, icecream   # 두개이상의 문자열의 집합은 리스트형태가 아니면 무조건 튜플이다.
+
+# 한문장에서 값을 교환하기 위해 임시변수를 사용하지 않고 튜플을 사용할 수 있다.
+password = 'swordfish'
+icecream = 'tuttifrutti'
+icecream, password = password, icecream
+icecream
+password
+
+# tuple()은 다른 객체를 튜플로 만들어준다.
+marx_list = ['Groucho', 'Chico', 'Harpo']
+tuple(marx_list)
