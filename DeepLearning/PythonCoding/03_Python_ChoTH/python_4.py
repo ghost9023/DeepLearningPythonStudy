20658d7c73e7af83b81720e51ddc6427d8756ad9
Administrator <meilleur7@gmail.com>
2017-07-20 09:44:38 +0900

    a

--- DeepLearning/PythonCoding/03_Python_ChoTH/python_4.py
+++ DeepLearning/PythonCoding/03_Python_ChoTH/python_4.py
@@ -303,4 +303,37 @@ print(comment)
 
 # 만약 함수가 명시적으로 return을 호출하지 않으면 호출자는 반환값으로 return을 얻는다.
 
+# 유용한 None
+thing = None
+if thing:
+    print("It's something")
+else:
+    print("It's nothing")
+# True는 아니지만 False도 아니다.
+
+if thing is None:
+    print("It's nothing")
+else:
+    print("It's something")
+
+def is_none(thing):
+    if thing is None:
+        print("It's None")
+    elif thing:
+        print("It's True")
+    else:
+        print("It's False")
+
+is_none(None)
+is_none(True)
+is_none(False)
+is_none(0)
+is_none(0.0)
+is_none(())
+is_none([])
+is_none({})
+is_none(set())
+
+
+
 
